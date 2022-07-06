5e6c58f139a5e953bddeba89cf8c1eccc275ffc5
Rishab Bahal <rishabbahal@gmail.com>
2022-07-07 00:08:59 +0530

    Signup form validations added to form inputs

--- src/app/body/signup/signup-form/signup-form.component.ts
+++ src/app/body/signup/signup-form/signup-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-signup-form',
@@ -11,15 +11,22 @@ export class SignupFormComponent implements OnInit {
   showError: boolean = false;
   errorMessage: String = '';
   signupSuccessful: Boolean = false;
+  showLoader: Boolean = false;
   constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {}
   signupForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
-    confirmPassword: new FormControl(''),
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+    confirmPassword: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
   });
 
   onSubmit() {
@@ -28,51 +35,33 @@ export class SignupFormComponent implements OnInit {
     const email = this.signupForm.get('email')?.value;
     const password = this.signupForm.get('password')?.value;
     const confirmPassword = this.signupForm.get('confirmPassword')?.value;
-    // Form validations
-    try {
-      if (firstName?.trim() == '') {
-        throw Error($localize`First name missing`);
-      } else if (lastName?.trim() == '') {
-        throw Error($localize`Last name missing`);
-      } else if (email?.trim() == '') {
-        throw Error($localize`Email missing`);
-      } else if (!email?.includes('@')) {
-        throw Error($localize`Invalid email`);
-      } else if (password?.trim() == '') {
-        throw Error($localize`Passsword missing`);
-      } else if (confirmPassword?.trim() == '') {
-        throw Error($localize`Please confirm the password`);
-      } else if (password != confirmPassword) {
-        throw Error($localize`Passwords don't match`);
-      } else {
-        this.showError = false;
-        this.errorMessage = '';
-        this.authService
-          .signup(
-            firstName ? firstName : '',
-            lastName ? lastName : '',
-            email ? email : '',
-            password ? password : '',
-          )
-          .subscribe({
-            next: (data) => {
-              console.log('data:');
-              console.log(data);
-              this.signupSuccessful = true;
-              setTimeout(() => {
-                this.router.navigate(['/']);
-              }, 3000);
-            },
-            error: (error) => {
-              this.showError = true;
-              this.errorMessage =
-              $localize`Couldn't register you. (Email might be already registered)`;
-            },
-          });
-      }
-    } catch (err: any) {
+
+    if (this.signupForm.valid) {
+      this.showLoader = true;
+      this.authService
+        .signup(
+          firstName ? firstName : '',
+          lastName ? lastName : '',
+          email ? email : '',
+          password ? password : ''
+        )
+        .subscribe({
+          next: (data) => {
+            this.showLoader = false;
+            this.signupSuccessful = true;
+            setTimeout(() => {
+              this.router.navigate(['/']);
+            }, 3000);
+          },
+          error: (error) => {
+            this.showLoader = false;
+            this.showError = true;
+            this.errorMessage = $localize`Couldn't register you. (Email might be already registered)`;
+          },
+        });
+    } else {
       this.showError = true;
-      this.errorMessage = err.message;
+      this.errorMessage = $localize`Form is invalid`;
     }
   }
 }
