d25241b87a3cb1d95459e79bf2fe427b88bea92c
Rishab Bahal <rishabbahal@gmail.com>
2022-07-15 07:31:39 +0530

    Auto redirecting from verify user page

--- src/app/body/verify-user/verify-user.component.ts
+++ src/app/body/verify-user/verify-user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -9,12 +9,14 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./verify-user.component.css'],
 })
 export class VerifyUserComponent implements OnInit {
+  timeCount: number = 8;
   invalidToken: boolean = false;
   isVerified: boolean = false;
   couldntVerify: boolean = false;
   email: string = '';
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private authService: AuthService
   ) {}
 
@@ -32,15 +34,26 @@ export class VerifyUserComponent implements OnInit {
               this.isVerified = false;
               this.couldntVerify = true;
             }
+            this.startRedirect();
           },
           error: (err) => {
             console.log(err);
             this.invalidToken = true;
             this.isVerified = false;
             this.couldntVerify = false;
+            this.startRedirect();
           },
         });
       },
     });
+  }
+
+  startRedirect() {
+    setInterval(() => {
+      this.timeCount--;
+      if (this.timeCount == 0) {
+        this.router.navigate(['/']);
+      }
+    }, 1000);
   }
 }
