0bb995863999a05193c2fb70befe89a344cf2dc5
Rishab Bahal <rishabbahal@gmail.com>
2022-07-18 14:38:03 +0530

    Validations added to login form.

--- src/app/body/login/login-form/login-form.component.ts
+++ src/app/body/login/login-form/login-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { AuthService } from 'src/app/services/auth.service';
@@ -26,51 +26,56 @@ export class LoginFormComponent implements OnInit {
     });
   }
   loginForm = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
   });
 
   onSubmit() {
-    const email = this.loginForm.get('email')?.value;
-    const password = this.loginForm.get('password')?.value;
-    this.authService
-      .login(email ? email : '', password ? password : '')
-      .subscribe({
-        next: (data: any) => {
-          // Save tokens in local storage
-          localStorage.setItem('refresh', data.refresh);
-          localStorage.setItem('access', data.access);
-          // Get user data from access token
-          this.authService.getUser().subscribe({
-            next: (userObj: any) => {
-              const loggedInUser = new User(
-                userObj.first_name,
-                userObj.last_name,
-                userObj.email,
-                userObj.gender,
-                userObj.date_of_birth,
-                userObj.profile_picture,
-                userObj.verified,
-                data.access
-              );
-              this.authService.updateAuthenticationState(loggedInUser, true);
-              this.showError = false;
-              this.errorMessage = '';
+    if (this.loginForm.valid) {
+      const email = this.loginForm.get('email')?.value;
+      const password = this.loginForm.get('password')?.value;
+      this.authService
+        .login(email ? email : '', password ? password : '')
+        .subscribe({
+          next: (data: any) => {
+            // Save tokens in local storage
+            localStorage.setItem('refresh', data.refresh);
+            localStorage.setItem('access', data.access);
+            // Get user data from access token
+            this.authService.getUser().subscribe({
+              next: (userObj: any) => {
+                const loggedInUser = new User(
+                  userObj.first_name,
+                  userObj.last_name,
+                  userObj.email,
+                  userObj.gender,
+                  userObj.date_of_birth,
+                  userObj.profile_picture,
+                  userObj.verified,
+                  data.access
+                );
+                this.authService.updateAuthenticationState(loggedInUser, true);
+                this.showError = false;
+                this.errorMessage = '';
 
-              this.router.navigate(['/']);
-            },
-            error: (err) => {
-              console.log(err);
-              this.showError = true;
-              this.errorMessage = $localize`Couldn't extract user data.`;
-            },
-          });
-        },
-        error: (error) => {
-          console.log(error);
-          this.showError = true;
-          this.errorMessage = $localize`Invalid credentials. Check email and password.`;
-        },
-      });
+                this.router.navigate(['/']);
+              },
+              error: (err) => {
+                console.log(err);
+                this.showError = true;
+                this.errorMessage = $localize`Couldn't extract user data.`;
+              },
+            });
+          },
+          error: (error) => {
+            console.log(error);
+            this.showError = true;
+            this.errorMessage = $localize`Invalid credentials. Check email and password.`;
+          },
+        });
+    }
   }
 }
