b2a3f96bc2b36212de16176c4dc03aa0d0afd1f9
Rishab Bahal <rishabbahal@gmail.com>
2024-04-05 12:14:12 -0400

    changed to react app

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  backend_url: 'http://localhost:8000',
-};
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import DiffViewerComponent from '../components/DiffViewerComponent';
+function Home() {
+    return (
+        <><h1>AI Resume Optimizer</h1><DiffViewerComponent /></>
+    );
+}
+
+export default Home;
