8e3c692691a91091bf019884558974b6fb19cba9
Rishab Bahal <rishabbahal@gmail.com>
2024-04-06 12:25:40 -0400

    basiuc frontend components added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,83 @@
-import React from 'react';
-import DiffViewerComponent from '../components/DiffViewerComponent';
+import React, { useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import DiffViewerComponent from "../components/DiffViewerComponent";
+
 function Home() {
-    return (
-        <><h1>AI Resume Optimizer</h1><DiffViewerComponent /></>
-    );
+  const [defaultResume, setDefaultResume] = useState();
+  const [responseLoaded, setResponseLoaded] = useState(false);
+
+  return (
+    <>
+      <h1>AI Resume Optimizer</h1>
+      <Form>
+        <Container fluid>
+          <Row>
+            <Col
+              xs={10}
+              md={5}
+              style={{
+                display: "block",
+                margin: "auto",
+              }}
+            >
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>Job description</Form.Label>
+                <Form.Control as="textarea" rows={15} />
+              </Form.Group>
+            </Col>
+            <Col
+              xs={10}
+              md={5}
+              style={{
+                display: "block",
+                margin: "auto",
+              }}
+            >
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>Current resume</Form.Label>
+                <Form.Control as="textarea" rows={15} />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col
+              xs={10}
+              md={5}
+              style={{
+                display: "block",
+                margin: "auto",
+              }}
+            ></Col>
+            <Col
+              xs={10}
+              md={5}
+              style={{
+                display: "block",
+                margin: "auto",
+              }}
+            >
+              <Button variant="outline-success">Save as default resume</Button>
+            </Col>
+          </Row>
+        </Container>
+        <div style={{ margin: "20px auto" }}>
+          <Button variant="primary">Optimize Resume</Button>
+        </div>
+      </Form>
+
+      <DiffViewerComponent />
+    </>
+  );
 }
 
 export default Home;
